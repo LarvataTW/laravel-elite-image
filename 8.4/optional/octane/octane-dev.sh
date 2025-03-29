7c8be411c09344be3a16bdd2543972bf62f29a41
crazylion <crazylion2@gmail.com>
2025-03-29 09:41:16 +0800

    fix:修正sh/bash 路徑問題

--- 8.4/optional/octane/octane-dev.sh
+++ 8.4/optional/octane/octane-dev.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 php /var/www/artisan octane:stop >/proc/1/fd/1
 rm -f /var/www/storage/framework/octane/octane-server-state.json /var/www/storage/logs/octane-server-state.json
